e84d347bcc5598f9fcf34263bc084184c6727e85
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-06 19:07:43 +0530

    employees

---
+++ src/main/java/com/example/springbootdbconnection/MyService.java
@@ -0,0 +1,10 @@
+package com.example.springbootdbconnection;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    public String getGreeting() {
+        return "Employee";
+    }
+}
---
+++ src/main/java/com/example/springbootdbconnection/SpringbootdbconnectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootdbconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdbconnectionApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootdbconnectionApplication.class, args);
+	}
+
+}
